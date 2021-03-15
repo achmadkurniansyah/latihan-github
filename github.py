fdcf386d499e3585d2f9e7e16c3ce237ed901219
achmadkurniansyah <65005697+achmadkurniansyah@users.noreply.github.com>
2021-03-15 09:51:09 +0700

    Ada Penambahan Perintah Baru
    Perintah yang ditambahkan adalah input untuk alas dan tinggi serta rumus luas segitiga

--- github.py
+++ github.py
@@ -1,2 +1,7 @@
 # Ini Hanya Sebuah Komentar
+alas = int(input("Masukan Alas : "))
+Tinggi = int(input("Masukan Tinggi : "))
+
+segitiga = 0.5*alas*tinggi
+
 print ('Halo ini Latihan Github Pertama')
